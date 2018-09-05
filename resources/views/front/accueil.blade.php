6c47f047c976541997646e1ef79478a652674564
Kevin Bataille <kevin.fxlan@gmail.com>
2018-09-05 15:07:21 +0200

    Avancée de l'affichage des news, décalage menu général vers la gauche

--- resources/views/front/accueil.blade.php
+++ resources/views/front/accueil.blade.php
@@ -1,11 +1,11 @@
 @extends("template")
 @section ('content')
-<div class="box">
-  <div id="gtco-portfolio" class="gtco-section">
+<div class="box text-center">
+  <div id="gtco-portfolio" class="gtco-section text-center">
       <div class="row">
         <div class="col-md-8 col-md-offset-2 text-center gtco-heading animate-box container gtco-container">
           <h2 class="text-center newsTitre">Nos derniers articles</h2>
-          <div class="news  gtco-container">
+          <div class="news  gtco-container text-center">
           @php($i=0)
           @foreach($tab_news as $laNews)
             @if($i<4)
@@ -16,15 +16,15 @@
               @endif
               <a id="{{ $laNews->id }}" class="color-1"  data-toggle="modal" data-target="#modalNews{{$laNews->id}}">
                 <div class="text-center mybox col-md-3 {{$esp}} animate-box">
-                  <h2>{{ $laNews->titre }}</h2> <br />
-                  <span>Contenu : {!! $laNews->contenu !!}</span> <br />
+                  <h2 style="font-size: 17px;">{{ $laNews->titre }}</h2> <br />
+                  <span>{!! str_limit($laNews->contenu , 25 , '... Voir plus.') !!}</span> <br /><br />
                   @if(auth::check())
                       @if($laNews->commentaires->count()==0)
-                        <span>Il n'y a pas de commentaire pour cette news</span>
+                        <span>0 <b class="ti-comment"></b></span>
                       @elseif ($laNews->commentaires->count()==1)
-                        <span>{!! $laNews->commentaires->count() !!} commentaire</span>
+                        <span>1 <b class="ti-comment"></b></span>
                       @else
-                        <span>{!! $laNews->commentaires->count() !!} commentaires</span>
+                        <span>{!! $laNews->commentaires->count() !!} <b class="ti-comments"></b></span>
                       @endif
                   @endif
                 </div>
@@ -106,4 +106,55 @@
     @php($i++)
     @endforeach
     <!-- FIN Modal de news -->
+
+        <!-- Modal de commentaire -->
+        @if(auth::check())
+      @foreach($tab_news as $laNews)
+      <div class="modal fade bd-example-modal-lg" id="modalCommentaire{{$laNews->id}}" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog modal-lg" role="document" style="width:75%;">
+          <div class="modal-content">
+
+            <!-- Main content -->
+            <div class="row">
+              <div class="col-md-12">
+                <div class="box box-info">
+                  {!! Form::open(['route' => "commentaire.store", 'method' => 'post', 'files' => true]) !!}
+                  <div class="box-header">
+                    <h3 class="box-title">  </h3>
+
+                    <div class="form-group">
+                      <label>Titre du commentaire :  </label>
+                      <input class="form-control" placeholder="Un titre" name="titre">
+                    </div>
+
+                  </div>
+                  <!-- /.box-header -->
+                  <div class="box-body pad">
+
+                    <div class="form-group">
+                      {{ Form::textarea('editor', '',['id'=>'editor','class'=>'form-control','placeholder'=>'Votre commentaire']) }}
+                    </div>
+
+                  </div>
+
+                  <button type="submit" class="btn btn-success btn-lg btn-block">Créer</button>
+                  <input class="form-control" name="id_news" value="{{$laNews->id}}" style="visibility:hidden">
+                  @if(Auth::check())
+                    @php ($pseudo = Auth::user()->nom ." ". Auth::user()->prenom)
+                    <input class="form-control" name="pseudo" value="{{$pseudo}}" style="visibility:hidden">
+                  @else
+                    @php ($pseudo = "ANONYME")
+                    <input class="form-control" name="pseudo" value="{{$pseudo}}" style="visibility:hidden">
+                  @endif
+                  {!! Form::close() !!}
+                </div>
+                <!-- /.box -->
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      @endforeach
+    @endif
+    <!-- FIN Modal de commentaire -->
   @stop
