78f04d8690441792f4f8d1031f195a45a21434bd
SebPern <ASUS PC@ASUS>
2018-09-05 13:09:07 +0200

    Nouvelle forme de l'accueil, toujours en cours de fabrication cependant Ajout des classes de styles qui vont avec dans le css, important

--- resources/views/front/accueil.blade.php
+++ resources/views/front/accueil.blade.php
@@ -1,53 +1,43 @@
 @extends("template")
 @section ('content')
-
-  <div class="box">
-
-    <!-- /.box-header -->
-
-
-    <div id="gtco-portfolio" class="gtco-section">
-      <div class="gtco-container">
-        <div class="row">
-          <div class="col-md-8 col-md-offset-2 text-center gtco-heading animate-box">
-            <h2>Nos derniers articles</h2>
-            <p>N'hésitez pas à aller voir nos albums photos !</p>
-          </div>
-        </div>
-
-        <div class="row row-pb-md">
-          <div class="col-md-12">
-            <ul id="gtco-portfolio-list">
-
-
-              @php($i=0)
-              @foreach($tab_news as $laNews)
-                @if($i <4)
-                <li class="animate-box" data-animate-effect="fadeIn" style="background-image: url({{$laNews->url}}); width:100%; ">
-                  <a id="{{ $laNews->id }}" class="color-1"  data-toggle="modal" data-target="#modalNews{{$laNews->id}}">
-                    <div class="case-studies-summary">
-                      <h2>{{ $laNews->titre }}</h2>
-                      <span>{!! $laNews->contenu !!}</span>
-                      @if(auth::check())
-                        @if($laNews->commentaires->count()==0)
-                          <span>Il n'y a pas de commentaire pour cette news</span>
-                        @elseif ($laNews->commentaires->count()==1)
-                          <span>{!! $laNews->commentaires->count() !!} commentaire</span>
-                        @else
-                          <span>{!! $laNews->commentaires->count() !!} commentaires</span>
-                        @endif
-                      @endif
-                    </div>
-                  </a>
-                </li>
+<div class="box">
+  <div id="gtco-portfolio" class="gtco-section">
+      <div class="row">
+        <div class="col-md-8 col-md-offset-2 text-center gtco-heading animate-box container gtco-container">
+          <h2 class="text-center newsTitre">Nos derniers articles</h2>
+          <div class="news  gtco-container">
+          @php($i=0)
+          @foreach($tab_news as $laNews)
+            @if($i<4)
+              @if($i == 1 || $i == 2)
+                @php($esp = "col-md-offset-1")
+              @else
+                @php($esp = "")
               @endif
-              @php($i++)
-              @endforeach
-            </ul>
+              <a id="{{ $laNews->id }}" class="color-1"  data-toggle="modal" data-target="#modalNews{{$laNews->id}}">
+                <div class="text-center mybox col-md-3 {{$esp}} animate-box">
+                  <h2>{{ $laNews->titre }}</h2> <br />
+                  <span>Contenu : {!! $laNews->contenu !!}</span> <br />
+                  @if(auth::check())
+                      @if($laNews->commentaires->count()==0)
+                        <span>Il n'y a pas de commentaire pour cette news</span>
+                      @elseif ($laNews->commentaires->count()==1)
+                        <span>{!! $laNews->commentaires->count() !!} commentaire</span>
+                      @else
+                        <span>{!! $laNews->commentaires->count() !!} commentaires</span>
+                      @endif
+                  @endif
+                </div>
+              </a>
+            @endif
+          @php($i++)
+          @endforeach 
           </div>
         </div>
-
       </div>
+  </div>
+</div>
+
 
 
       <!-- Modal de  news -->
@@ -116,57 +106,4 @@
     @php($i++)
     @endforeach
     <!-- FIN Modal de news -->
-
-
-    <!-- Modal de commentaire -->
-    @if(auth::check())
-      @foreach($tab_news as $laNews)
-      <div class="modal fade bd-example-modal-lg" id="modalCommentaire{{$laNews->id}}" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-        <div class="modal-dialog modal-lg" role="document" style="width:75%;">
-          <div class="modal-content">
-
-            <!-- Main content -->
-            <div class="row">
-              <div class="col-md-12">
-                <div class="box box-info">
-                  {!! Form::open(['route' => "commentaire.store", 'method' => 'post', 'files' => true]) !!}
-                  <div class="box-header">
-                    <h3 class="box-title">  </h3>
-
-                    <div class="form-group">
-                      <label>Titre du commentaire :  </label>
-                      <input class="form-control" placeholder="Un titre" name="titre">
-                    </div>
-
-                  </div>
-                  <!-- /.box-header -->
-                  <div class="box-body pad">
-
-                    <div class="form-group">
-                      {{ Form::textarea('editor', '',['id'=>'editor','class'=>'form-control','placeholder'=>'Votre commentaire']) }}
-                    </div>
-
-                  </div>
-
-                  <button type="submit" class="btn btn-success btn-lg btn-block">Créer</button>
-                  <input class="form-control" name="id_news" value="{{$laNews->id}}" style="visibility:hidden">
-                  @if(Auth::check())
-                    @php ($pseudo = Auth::user()->nom ." ". Auth::user()->prenom)
-                    <input class="form-control" name="pseudo" value="{{$pseudo}}" style="visibility:hidden">
-                  @else
-                    @php ($pseudo = "ANONYME")
-                    <input class="form-control" name="pseudo" value="{{$pseudo}}" style="visibility:hidden">
-                  @endif
-                  {!! Form::close() !!}
-                </div>
-                <!-- /.box -->
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-      @endforeach
-    @endif
-    <!-- FIN Modal de commentaire -->
-
   @stop
