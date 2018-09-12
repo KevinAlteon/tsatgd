ddf2bbadeb5169886377d91f63e5f88010b0d752
Kevin Bataille <kevin.fxlan@gmail.com>
2018-09-12 16:59:09 +0200

    Amélioration de la galerie à l'intérieur d'un album, corrections à venir

--- resources/views/front/galerie.blade.php
+++ resources/views/front/galerie.blade.php
@@ -5,21 +5,41 @@
 @section ('content')
 <!-- QUAND ON A CLIQUE SUR UNE MINIATURE -->
 @if (isset($images))
-  <div class="container">
-    <div class="row">
-      <div class="text-center col-md-12">
-        <h2>Album : {{$titreAlbum}}</h2><br />
+<div class="container">
+  <div class="row">
+    <div class="text-center col-md-12">
+      <h2>Album : <b>{{$titreAlbum}}</b></h2><br />
+    </div>
+    @php($x = 0)
+    @foreach ($images as $uneImage)
+    <div class="gallery_product col-lg-3 col-md-3 col-sm-4 col-xs-3 pointer">
+      <div class="overlay-image">
+        <img src="{{url('files/')."/".$uneImage}}" class="img-responsive" style="height: 265px; width:265px;">
+        <div class="hover" id="image{{$x}}" data-toggle="modal" data-target="#imagemodal{{$x}}">
+        <div class="text">
+          Agrandir
+        </div>  
       </div>
-      @foreach($images as $uneImage)
-      <div class="gallery_product col-md-6">
-        <img src="{{url('files/')."/".$uneImage}}" style="width: 100%;">
-      </div>
-      @endforeach
   </div>
-  <a href="{{route('galerie')}}">Retour</a>
+</div>
+@php($x++)
+@endforeach
+<!-- FIN ON A CLIQUE SUR UNE MINIATURE -->
+<!-- DEBUT MODALE AGRANDIR IMAGE -->
+@php($x = 0)
+@foreach($images as $uneImage)
+  <div class="modal" tabindex="-1" role="dialog" id="imagemodal{{$x}}">
+    <div class="modal-dialog modal-lg" role="document">
+      <div class="modal-content">
+        <div class="modal-body">
+          <img src="{{url('files/')."/".$uneImage}}" class="img-responsive">
+        </div>
+      </div>
     </div>
   </div>
-<!-- FIN ON A CLIQUE SUR UNE MINIATURE -->
+  @php($x++)
+@endforeach
+<!-- FIN MODALE AGRANDIR IMAGE -->
 @endif
 @if (isset($lesAlbums))
 <!-- DEBUT  MINIATURE -->
